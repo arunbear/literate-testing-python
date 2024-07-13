bfa3ccfa550429f59f27919cafbf574d9dc31a57
Arun Prasaad <arunbear@cpan.org>
2024-07-13 15:38:25 +0100

    Add test case: An empty stack throws when queried for its top item

--- stack.py
+++ stack.py
@@ -1,3 +1,15 @@
 class Stack:
+    class InvalidStateError(Exception):
+        pass
+
+    def __init__(self):
+        self.__stack = []
+
     def depth(self):
         return 0
+
+    def top(self):
+        if len(self.__stack) == 0:
+            raise Stack.InvalidStateError
+
+        return self.__stack[-1]
--- tests/stack_spec.py
+++ tests/stack_spec.py
@@ -1,3 +1,5 @@
+import pytest
+
 from stack import Stack
 
 class StackSpec:
@@ -5,3 +7,9 @@ class StackSpec:
         def is_empty(self):
             stack = Stack()
             assert stack.depth() == 0
+
+    class An_empty_stack:
+        def throws_when_queried_for_its_top_item(self):
+            with pytest.raises(Stack.InvalidStateError):
+                stack = Stack()
+                stack.top()
