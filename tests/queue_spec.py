8ee6e3d6bd229fdfe5aad6b0cc2bea41d90b71a1
Arun Prasaad <arunbear@cpan.org>
2024-07-14 18:02:22 +0100

    Add test case: A new queue preserves positive bounding capacity

--- tests/queue_spec.py
+++ tests/queue_spec.py
@@ -1,13 +1,22 @@
 class Queue:
-    def __init__(self):
+    def __init__(self, capacity):
+        self.__capacity = capacity
         self.__queue = []
 
     def length(self):
         return 0
 
+    def capacity(self):
+        return self.__capacity
+
 
 class QueueSpec:
     class A_new_queue:
         def is_empty(self):
-            queue = Queue()
+            queue = Queue(1)
             assert queue.length() == 0
+
+        def preserves_positive_bounding_capacity(self):
+            capacity = 1
+            queue = Queue(capacity)
+            assert queue.capacity() == capacity
